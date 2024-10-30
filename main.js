f67d37761ede6b3aceac7a888922b09420f58286
Jacob Sorrells <Sorreljh@dukes.jmu.edu>
2024-10-30 13:17:34 -0400

    I finished implementing the code

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ function updateRadio(options) {
   form.innerHTML = yous
 }
 
-// this ends the little bit you shoudln't need to edit.
+// this ends the little bit you shouldn't need to edit.
 ///////////////////////////////////////////////////////////////////////////////
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -47,24 +47,40 @@ function init(ev) {
   // FIXME: notice above that getYous just returns a literal.
   // you should update the code below to instead call getOptions.
   // getOptions expects no arguments, and returns a promise that resolves to an array of strings.
-  const options = getYous()
-  updateRadio(options)
 
-  document.querySelectorAll("input[type='radio']").forEach((input) => {
-    input.addEventListener('change', changed);
+  toggleLoader('you'); 
+
+  getOptions().then((options) => {
+    updateRadio(options);
+
+    toggleLoader('you'); 
+
+    // Attach event listeners after the radio buttons have been created
+    document.querySelectorAll("input[type='radio']").forEach((input) => {
+      input.addEventListener('change', changed);
+    });
   });
+
 }
 
 function changed(ev) {
   console.debug('fyi, this is what a change event looks like', ev)
   const you = ev.target.parentElement.textContent
 
-  // FIXME: notice above that getThemProblem just returns a literal.
+  // FIXME: notice above that getThey just returns a literal.
   // you should update the code below to instead call getThemProblem.
   // getThemProblem expects a string parameter (the only valid strings are those returned by getOptions), and returns a promise that resolves to a string.
-  const they = getThey(you)
-  const output = document.getElementById('they')
-  output.textContent = they
+  const output = document.getElementById('they');
+  output.textContent = '';
+  
+  toggleLoader('they'); 
+  getThemProblem(you).then((they) => {
+    const output = document.getElementById('they')
+    output.textContent = they
+
+    toggleLoader('they');
+  });
+
 }
 
 document.addEventListener("DOMContentLoaded", init);
